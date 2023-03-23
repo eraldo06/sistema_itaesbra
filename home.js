dd6cfa5c64d00b0b7095ab8ac06581c494e84cd8
Eraldo Leonardo <99341827+eraldo06@users.noreply.github.com>
2023-03-22 21:53:37 -0300

    criando interface
    criando interface

---
+++ home.js
@@ -0,0 +1,55 @@
+function rota() {
+    let arrow = document.querySelector(".animation");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto');
+    arrow2.classList.toggle('ativa')
+}
+
+function rota2() {
+    let arrow = document.querySelector(".animation2");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto2');
+    arrow2.classList.toggle('ativa')
+}
+function rota3() {
+    let arrow = document.querySelector(".animation3");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto3');
+    arrow2.classList.toggle('ativa')
+}
+function rota4() {
+    let arrow = document.querySelector(".animation4");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto4');
+    arrow2.classList.toggle('ativa')
+}
+function rota5() {
+    let arrow = document.querySelector(".animation5");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto5');
+    arrow2.classList.toggle('ativa')
+}
+function rota6() {
+    let arrow = document.querySelector(".animation6");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto6');
+    arrow2.classList.toggle('ativa')
+}
+function rota7() {
+    let arrow = document.querySelector(".animation7");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto7');
+    arrow2.classList.toggle('ativa')
+}
+function rota8() {
+    let arrow = document.querySelector(".animation8");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto8');
+    arrow2.classList.toggle('ativa')
+}
+function rota9() {
+    let arrow = document.querySelector(".animation9");
+    arrow.classList.toggle('seta')
+    var arrow2 = document.querySelector('.texto9');
+    arrow2.classList.toggle('ativa')
+}
